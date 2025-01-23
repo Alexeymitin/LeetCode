77e96bf09e3dffd3653fbacfb686269a7a052882
Alexey Mitin <90648444+Alexeymitin@users.noreply.github.com>
2025-01-23 21:14:13 +0300

    Time: 0 ms (100.00%) | Memory: 48.2 MB (98.88%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -7,9 +7,9 @@
  */
 var merge = function(nums1, m, nums2, n) {
 
-    p1 = m - 1;
-    p2 = n - 1;
-    p3 = m + n -1;
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p3 = m + n -1;
 
     while (p1 >= 0 && p2 >= 0) {
         if (nums1[p1] > nums2[p2]) {
