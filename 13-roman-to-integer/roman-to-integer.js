aa8c38b81af7459283e547defdbb6c2c4e516dea
Alexey Mitin <90648444+Alexeymitin@users.noreply.github.com>
2025-01-29 23:18:52 +0300

    Time: 5 ms (79.91%) | Memory: 54.9 MB (73.60%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    let number = 0;
+
+    for (let i = 0; i < s.length; i++) {
+    
+      if (i + 1 < s.length && dict[s[i]] < dict[s[i + 1]]) {
+         number -= dict[s[i]]
+      } else {
+        number += dict[s[i]]
+      }         
+    }
+
+    return number;
+};
