1b88dbfe4184fabe7b79c0ac6a99c9994e98e6f0
Alexey Mitin <90648444+Alexeymitin@users.noreply.github.com>
2025-02-12 21:45:29 +0300

    Time: 1 ms (90.26%) | Memory: 51.2 MB (38.08%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
- let pointer = 1
- for (let i = 0; i < nums.length; i++) {
-    for (let j = pointer; j < nums.length; j++) {
-      if (target === nums[i] + nums[j]) {
-        return [i, j];
-      }
+    let map = new Map(); // Хеш-таблица для хранения чисел и их индексов
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]; // Вычисляем, какое число нужно найти
+        
+        // Если дополнение уже есть в хеш-таблице, возвращаем индексы
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        // Иначе, добавляем текущее число и его индекс в хеш-таблицу
+        map.set(nums[i], i);
     }
-    pointer++
-  }
 };
