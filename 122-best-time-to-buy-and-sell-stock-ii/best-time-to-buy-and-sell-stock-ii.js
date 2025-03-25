4901644e5c79eef7755608e3daa92cf7ae718d6e
Alexey Mitin <90648444+Alexeymitin@users.noreply.github.com>
2025-03-25 20:39:26 +0300

    Time: 2 ms (33.92%) | Memory: 53.9 MB (39.89%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,22 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = prices[0];
     let maxProfit = 0;
     let p1 = 0;
-    let p2 = 1;
+  
 
     while (p1 < prices.length) {
+        if (prices[p1 + 1] - prices[p1] > 0) {
+            maxProfit = prices[p1 + 1] - prices[p1] + maxProfit
+           
+        } 
         
-        if (prices[p2] - prices[p1] > 0) {
-            maxProfit = prices[p2] - prices[p1] + maxProfit
-            p1++
-            p2++
-        } else {
-            p1++
-            p2++
-        }
-      
+       p1++
     }
 
     return maxProfit;
