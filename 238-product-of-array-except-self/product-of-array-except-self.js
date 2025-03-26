6107fe47f9c42b7599dae9597d8e0050d3cef459
Alexey Mitin <90648444+Alexeymitin@users.noreply.github.com>
2025-03-26 22:06:24 +0300

    Time: 1 ms (99.39%) | Memory: 69.9 MB (40.95%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,19 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const n = nums.length;
-    const result = new Array(n).fill(1);
+  const result = [];  // Массив для хранения результата
 
-    // Переменная для произведения слева
     let left = 1;
-    for (let i = 0; i < n; i++) {
-        result[i] *= left;  // Умножаем на произведение слева
-        left *= nums[i];     // Обновляем произведение слева
+    for (let i = 0; i < nums.length; i++) {
+        result.push(left);  // Добавляем произведение слева
+        left *= nums[i];    // Обновляем произведение слева
     }
 
-    // Переменная для произведения справа
     let right = 1;
-    for (let i = n - 1; i >= 0; i--) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         result[i] *= right;  // Умножаем на произведение справа
         right *= nums[i];     // Обновляем произведение справа
     }
