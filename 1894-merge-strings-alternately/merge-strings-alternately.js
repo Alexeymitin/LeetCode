0aeda1bcaaa127edaedd09b55b9b425a95e25153
Alexey Mitin <90648444+Alexeymitin@users.noreply.github.com>
2025-03-26 22:39:18 +0300

    Time: 49 ms (47.88%) | Memory: 55.8 MB (5.48%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,26 +4,14 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let p1 = 0;
-    let p2 = 0;
-    let mergedString = ""
+    let result = "";
+    let i = 0;
 
-    while (p1 < word1.length && p2 < word2.length) {
-        mergedString += word1[p1] + word2[p2];
-        p1++
-        p2++
+    while (i < word1.length || i < word2.length) {
+        if (i < word1.length) result += word1[i];
+        if (i < word2.length) result += word2[i];
+        i++;
     }
 
-
-    while (p2 < word2.length) {
-        mergedString += word2[p2]
-        p2++
-    }
-
-     while (p1 < word1.length) {
-        mergedString += word1[p1]
-        p1++
-    }
-
-    return mergedString;
+    return result;
 };     
